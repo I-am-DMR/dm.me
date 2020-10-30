55c9146314085d9a5dc66bcc025ee261987c1215
Devamakesh <devamakeshhr19mcs006@skasc.ac.in>
2020-10-30 11:57:07 +0530

    change (v 1.0.8)

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,106 +1,47 @@
 import React from 'react'
-import ButtonA from './Button/ButtonA'
-import Nav from './nav'
+import Grid from './grid'
+import Skill from './skill'
 export default function Home() {
     return (
         <div>
            <main className="page lanidng-page">
-           <section className="portfolio-block block-intro">
+           <section
+  className="portfolio-block block-intro"
+  style={{ color: "rgb(58,171,165)", borderColor: "rgba(86,181,176,0)" }}
+>
   <div
     className="rubberBand animated avatar"
     style={{ backgroundImage: 'url("assets/img/avatars/avatar.jpg")' }}
   />
   <div className="container">
     <div className="about-me">
-      <p>
+      <p
+        style={{
+          fontFamily: "ABeeZee, sans-serif",
+          borderColor: "rgb(0,0,0)",
+          color: "rgb(0,0,0)"
+        }}
+      >
         Hello! I am DEVA MAKEH. I work as platform developer. I have passion for
         pixel perfect, minimal and easy to use interfaces.
       </p>
-      <a className="btn btn-outline-primary" role="button" href="https://www.linkedin.com/in/devamakeshr/">
+      <a
+        className="btn btn-outline-primary"
+        role="button"
+        href="https://www.linkedin.com/in/devamakeshr/"
+        style={{
+          background: "#eb1b72",
+          borderColor: "rgb(255,255,255)",
+          color: "rgb(255,255,255)"
+        }}
+      >
         Hire me
       </a>
     </div>
   </div>
 </section>
-
-  <section className="portfolio-block photography">
-    <div className="container">
-      <div className="row no-gutters">
-        <div className="col-md-6 col-lg-4 item zoom-on-hover">
-          <a href="#">
-            <img
-              className="img-fluid image"
-              src="assets/img/nature/IMG_0765.png"
-            />
-          </a>
-        </div>
-        <div className="col-md-6 col-lg-4 item zoom-on-hover">
-          <a href="#">
-            <img
-              className="img-fluid image"
-              src="assets/img/nature/IMG_5175.jpg"
-            />
-          </a>
-        </div>
-        <div className="col-md-6 col-lg-4 item zoom-on-hover">
-          <a href="#">
-            <img
-              className="img-fluid image"
-              src="assets/img/tech/IMG_5176.jpg"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section className="portfolio-block call-to-action border-bottom">
-    <div className="container">
-      <div className="d-flex justify-content-center align-items-center content">
-        <h3>Like what you see?</h3>
-        <ButtonA Name="Hire me"/>
-      </div>
-    </div>
-  </section>
-  <section className="portfolio-block skills">
-    <div className="container">
-      <div className="heading">
-        <h2>Special Skills</h2>
-      </div>
-      <div className="row">
-        <div className="col-md-4">
-          <div className="card special-skill-item border-0">
-            <div className="card-header bg-transparent border-0">
-              <i className="icon ion-ios-star-outline" />
-            </div>
-            <div className="card-body">
-              <h3 className="card-title">Web Design</h3>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card special-skill-item border-0">
-            <div className="card-header bg-transparent border-0">
-              <i className="icon ion-ios-lightbulb-outline" />
-            </div>
-            <div className="card-body">
-              <h3 className="card-title">Interface Design</h3>
-              <p className="card-text" />
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card special-skill-item border-0">
-            <div className="card-header bg-transparent border-0">
-              <i className="icon ion-ios-gear-outline" />
-            </div>
-            <div className="card-body">
-              <h3 className="card-title">Linux Specialist</h3>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+<Grid/>
+<Skill/>
 </main>
 
         </div>
---
+++ src/components/skill.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+export default function skill() {
+    return (
+        <div>
+            <section
+  className="portfolio-block skills"
+  style={{ background: "rgba(40,181,225,0.6)" }}
+>
+  <div className="container">
+    <div className="heading">
+      <h2>Special Skills</h2>
+    </div>
+    <div className="row">
+      <div className="col-md-4">
+        <div className="card special-skill-item border-0">
+          <div className="card-header bg-transparent border-0">
+            <i
+              className="icon ion-ios-star-outline"
+              style={{ background: "rgb(222,27,27)" }}
+            />
+          </div>
+          <div className="card-body">
+            <h3 className="card-title">Web Design</h3>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4" style={{ background: "rgba(0,0,0,0)" }}>
+        <div className="card special-skill-item border-0">
+          <div className="card-header bg-transparent border-0">
+            <i
+              className="icon ion-ios-lightbulb-outline"
+              style={{ background: "rgb(222,166,20)" }}
+            />
+          </div>
+          <div
+            className="card-body"
+            style={{ background: "rgba(255,97,97,0)" }}
+          >
+            <h3 className="card-title">Interface Design</h3>
+            <p className="card-text" />
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="card special-skill-item border-0">
+          <div className="card-header bg-transparent border-0">
+            <i
+              className="icon ion-ios-gear-outline"
+              style={{ background: "rgb(20,222,149)" }}
+            />
+          </div>
+          <div className="card-body">
+            <h3 className="card-title">Linux&nbsp;Specialist</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="d-flex justify-content-center align-items-center content">
+    <h3>Like what you see?</h3>
+    <button
+      className="btn btn-outline-primary btn-lg"
+      type="button"
+      style={{
+        borderColor: "rgb(233,248,59)",
+        color: "rgb(23,23,23)",
+        background: "#f7f4f4"
+      }}
+    >
+      Hire me
+    </button>
+  </div>
+</section>
+
+        </div>
+    )
+}

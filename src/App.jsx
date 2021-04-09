c9319240d6f05996c02b6d3c393c25557748950f
deva <devamakeshramasamy@gmail.com>
2021-04-09 18:34:25 +0530

    v-1.1.0 add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import Body from "./components/home";
-import Nav from "./screen/nav";
-import Footer from "./components/footer";
+import React from 'react';
+import Body from './components/home';
+import Nav from './screen/nav';
+import ReactGA from 'react-ga';
+import Footer from './components/footer';
 export default function App() {
   return (
     <div className="">
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import withTracker from './helpers/withTracker';
+import { BrowserRouter, Route } from 'react-router-dom';
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Route
+      component={withTracker(App, {
+        /* additional attributes */
+      })}
+    />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );

cfdf2f5b30d7423ba32658456e5f04420b51b85e
Devamakesh <59350409+mrdm-me@users.noreply.github.com>
2021-06-13 12:25:14 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,10 +13,10 @@ export default function footer() {
             <a href="https://www.facebook.com/devamakeshr">
               <i className="icon ion-social-facebook" />
             </a>
-            <a href="https://www.instagram.com/devamakesh.me">
+            <a href="https://www.instagram.com/mrdm.me/">
               <i className="icon ion-social-instagram-outline" />
             </a>
-            <a href="https://twitter.com/devamakeshr">
+            <a href="https://twitter.com/mrdm_me">
               <i className="icon ion-social-twitter" />
             </a>
           </div>
